8ff3658bc4f34ae78791356f4df06e13192211fa
Dzmitry Struneuski <isidbrest@gmail.com>
2022-06-23 10:54:36 +0300

    cleardata_done

--- cleardata.sh
+++ cleardata.sh
@@ -12,4 +12,11 @@ else
     sed -i '/^$/d' $InputPath
 # Leave lines start with registration number and okrb
     awk '/(^.{15}\s.{5}\s.*[0-9]{5}$)|(^.{3}\s.{4}\s[0-9]{2})/' $InputPath > $OutputPath
+sed -i -e 's/Регистрационный номер	//g' \
+       -e 's/Код ОКРБ	//g' \
+$OutputFileName
+# Print different files with different info inside
+awk 'BEGIN {FS=" "} /^[0-9]{2}/ {print $1}' $OutputPath > okrb.txt
+awk 'BEGIN {FS=" "} /[0-9]{5}$/ {print}' $OutputPath > regnumbers.txt
+rm clear.txt
 fi
