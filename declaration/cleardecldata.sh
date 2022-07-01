14ea6291c3d554dea4a9d8de89b67e9c865521e5
Dzmitry Struneuski <isidbrest@gmail.com>
2022-07-01 14:54:22 +0300

    add_objects

--- declaration/cleardecldata.sh
+++ declaration/cleardecldata.sh
@@ -11,8 +11,11 @@ else
     Regnumbers="$1/regnumbers.txt"
     Days="$1/days.txt"
     Applicants="$1/applicants.txt"
+    Objects="$1/objects.txt"
 # Remove empty lines
     sed -i '/^$/d' $InputPath
+# Replace text with new line symbols 
+    perl -i -p -e 's/(Объект оценки соответствия №[1]\n)|(Объект оценки соответствия №[1]\s\n)/Объект оценки соответствия /g' $InputPath
 # Clear data.txt with different conditions
 awk 'BEGIN {FS=" "} /^.{15}\s.{5}\s.*[0-9]{5}$/ {print}' $InputPath > $Regnumbers
 sed -i 's/Регистрационный номер	//g' $Regnumbers
@@ -21,7 +24,10 @@ sed -i 's/2.4. Дата регистрации	//g' $Days
 awk 'BEGIN {FS=" "} /^.{12}\s.{14}\s.{8}\s.*$/ {print}' $InputPath > $Applicants
 sed -i 's/Наименование хозяйствующего субъекта	//g' $Applicants
 sed -i '/^$/d' $Applicants
+awk 'BEGIN {FS=" "} /^.{6}\s.{6}\s.{12}\s.{12}\s.{7}\s.{6}\s.{12}\s.*$/ {print}' $InputPath > $Objects
+sed -i 's/Объект оценки соответствия Наименование объекта оценки соответствия	//g' $Objects
+sed -i '/^$/d' $Objects
 # Combine data into a single CSV-file
-paste -d'\t' $Regnumbers $Days $Applicants > $OutputFileName
-rm $Regnumbers $Days $Applicants
+paste -d'\t' $Regnumbers $Days $Applicants $Objects > $OutputFileName
+rm $Regnumbers $Days $Applicants $Objects
 fi
