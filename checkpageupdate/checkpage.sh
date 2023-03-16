885198590dcf9dec0a5a810ca7d547b53800e554
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-03-16 15:18:15 +0300

    add timer

--- checkpageupdate/checkpage.sh
+++ checkpageupdate/checkpage.sh
@@ -6,8 +6,11 @@ url="https://medic-service.by/BrestGDP3/ticketGet/?room_id=1573"
 # Set the text to search for in the web page content
 text_to_search="нет доступных талонов"
 
+# Set the maximum time to wait for the web page to load in seconds
+timeout_seconds=10
+
 # Fetch the content of the web page and search for the text
-if curl -s "$url" | grep -q "$text_to_search"; then
+if curl -s --max-time "$timeout_seconds" "$url" | grep -q "$text_to_search"; then
   echo "The web page contains the text '$text_to_search'"
 else
   echo "The web page does not contain the text '$text_to_search'"
