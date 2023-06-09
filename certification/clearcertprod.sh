2e4322819d52763bbcdd854cd59fc9c035c821a9
sid-brest <isidbrest@gmail.com>
2023-06-09 12:30:34 +0300

    add corrections

--- certification/clearcertprod.sh
+++ certification/clearcertprod.sh
@@ -29,16 +29,15 @@ Okp="$path/okp.txt"
 sed -nE '/Регистрационный номер:[ \t]/s/Регистрационный номер:[ \t]//p' "$InputPath" | sed 's/[[:space:]]\+$//' > "$Regnumbers"
 sed -nE '/2\.4\. Дата регистрации/s/2\.4\. Дата регистрации//p' "$InputPath" | sed -E 's/[^0-9\.]+//g' > "$Days"
 sed -nE '/2\.5\. Срок действия/s/2\.5\. Срок действия//p' "$InputPath" | sed -E 's/[^0-9\.]+//g' > "$Period"
-sed -n 's/.*объект выполнения работ - \(.*\), .*/\1/p' "$InputPath" | sed 's/, .*//g' > "$Objects"
+sed -nE '/Информация об использовании продукции*./,/, расположен по адресу /{//!p}' "$InputPath" > "$Objects"
 sed -nE '/Краткое наименование хозяйствующего субъекта[ \t]/s/Краткое наименование хозяйствующего субъекта[ \t]//p' "$InputPath" | sed -E 's/ +([a-zA-Z])/\1/g' | sed '/^\s*$/d' > "$Applicants"
 sed -nE '/Наименование хозяйствующего субъекта[ \t]/s/Наименование хозяйствующего субъекта[ \t]//p' "$InputPath" | sed -E 's/ +([a-zA-Z])/\1/g' | sed '/^\s*$/d' > "$Fullapplicants"
 sed -nE '/2\.7\. Типографский номер бланка сертификата[ \t]/s/2\.7\. Типографский номер бланка сертификата[ \t]//p' "$InputPath" | sed 's/[^0-9]+//g' > "$Blank"
-sed -nE '/(Объект|Наименование) объекта оценки соответствия[ \t]/s/(Объект|Наименование) объекта оценки соответствия[ \t]//p' "$InputPath" | sed -E 's/ +([a-zA-Z])/\1/g' | sed '/^\s*$/d' > "$Production"
+sed -nE '/(Объект оценки соответствия №1|Иные сведения об объекте оценки соответствия, обеспечивающие его идентификацию[ \t])/s/(Объект оценки соответствия №1|Иные сведения об объекте оценки соответствия, обеспечивающие его идентификацию[ \t])//p' "$InputPath" | sed -E 's/ +([a-zA-Z])/\1/g' | sed '/^\s*$/d' > "$Production"
 sed -nE '/Иные сведения об объекте оценки соответствия, обеспечивающие его идентификацию/s/Иные сведения об объекте оценки соответствия, обеспечивающие его идентификацию//p; /./!d; N; s/\n/ /' "$InputPath" | sed 's/^[ \t]*//' > "$Otherinfo"
 
 # Combine data into a single CSV-file.
  paste -d'\t' "$Regnumbers" "$Blank" "$Days" "$Period" "$Applicants" "$Fullapplicants" "$Objects" <(paste -d' ' "$Production" "$Otherinfo") > "$OutputPath" 
- paste -d'\t' "$Regnumbers" "$Blank" "$Days" "$Period" "$Applicants" "$Fullapplicants" "$Objects" <(paste -d' ' "$Production" "$Otherinfo") | iconv -f UTF-8 -t windows-1251 > "$OutputPathRu"
-
+ 
 # Remove temporary files.
-rm "$Regnumbers" "$Blank" "$Days" "$Period" "$Applicants" "$Fullapplicants" "$Objects" "$Production" "$Otherinfo"
+# rm "$Regnumbers" "$Blank" "$Days" "$Period" "$Applicants" "$Fullapplicants" "$Objects" "$Production" "$Otherinfo"
