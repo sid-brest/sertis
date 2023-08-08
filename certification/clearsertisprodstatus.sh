d1750f69bebc78b89e74ca64f676233236ad3dda
sid-brest <isidbrest@gmail.com>
2023-08-08 10:58:18 +0300

    add address filter

--- certification/clearsertisprodstatus.sh
+++ certification/clearsertisprodstatus.sh
@@ -47,7 +47,36 @@ sed -nE '/2\.7\. Типографский номер бланка сертифи
 sed -nE '/Информация об использовании продукции*./,/, расположен по адресу /{//!p}' "$InputPath" > "$Objects"
 sed -nE '/Краткое наименование хозяйствующего субъекта[ \t]/s/Краткое наименование хозяйствующего субъекта[ \t]//p' "$InputPath" | sed -E 's/ +([a-zA-Z])/\1/g' | sed '/^\s*$/d' > "$Applicants"
 sed -nE '/Наименование хозяйствующего субъекта[ \t]/s/Наименование хозяйствующего субъекта[ \t]//p' "$InputPath" | sed -E 's/ +([a-zA-Z])/\1/g' | sed '/^\s*$/d' > "$Fullapplicants"
-# (?) sed -nE '/Адрес заявителя /s/Адрес заявителя //p' "$InputPath" | sed -E 's/(Адрес заявителя|Место нахождения|адрес|юр. лица|Область|Город|Улица|Номер дома|Почтовый индекс)//g; s/[:,()]//g; s/\s+/ /g; s/^ //' > "$Address"
+
+
+# Focus on Address.
+awk 'BEGIN {FS=" "} /^Адрес заявителя.*$/ {print}' "$InputPath" > "$Address"
+sed -i 's/Адрес заявителя	(BY) Беларусь, (1) Место нахождения (адрес юр. лица), //g' "$Address"
+sed -i 's/^.*Адрес в текстовой форме//g' "$Address"
+sed -i 's/ Республика Беларусь//g' "$Address"
+sed -i 's/^\://g' "$Address"
+sed -i 's/^\,//g' "$Address"
+sed -i 's/^\ //g' "$Address"
+sed -i 's/: / /g' "$Address"
+sed -i 's/Номер дома //g' "$Address"
+sed -i 's/Город /г. /g' "$Address"
+sed -i 's/Улица /ул. /g' "$Address"
+sed -i 's/Номер помещения / /g' "$Address"
+sed -i 's/ Населенный пункт / /g' "$Address"
+sed -i 's/Область Минск, //g' "$Address"
+sed -i 's/сельский совет/c\/c/g' "$Address"
+sed -i 's/агрогородок/аг\./g' "$Address"
+sed -i 's/агрогородка/аг\./g' "$Address"
+sed -i 's/область/обл\./g' "$Address"
+sed -i 's/район/р\-н/g' "$Address"
+sed -i 's/ Номер абонентского ящика.*//g' "$Address"
+sed -i 's/ Почтовый//g' "$Address"
+sed -i 's/\s*$//g' "$Address"
+sed -i 's/\,*$//g' "$Address"
+sed -i 's/ *| */|/g' "$Address"
+sed -i 's/^\(Область\) \([^ ,]*\)/\2 \1/' "$Address"
+sed -i 's/Область/обл\./g' "$Address"
+# sed -nE '/Адрес заявителя /s/Адрес заявителя //p' "$InputPath" | sed -E 's/(Адрес заявителя|Место нахождения|адрес|юр. лица|Область|Город|Улица|Номер дома|Почтовый индекс)//g; s/[:,()]//g; s/\s+/ /g; s/^ //' > "$Address"
 # sed -nE '/ФИО[ \t]/s/ФИО[ \t]//p' "$InputPath" | awk '{print $1, substr($2,1,1) "." substr($3,1,1) "."}' | awk '{split($0,a," "); printf "%s%s\n", a[2], a[1]}' > "$NameHead"
 # sed -nE 'Дата документа/s/Дата документа//p' "$InputPath" | sed -E 's/[^0-9\.]+//g' > "$DocumentDate"
 # sed -nE '/Идентификатор хозяйствующего субъекта/s/Идентификатор хозяйствующего субъекта//p' "$InputPath" | sed -E 's/[^0-9]+//g' > "$ID"
@@ -66,7 +95,7 @@ sed -nE '/Код товара по ТН ВЭД ЕАЭС/s/Код товара п
 paste -d' ' "$Production" "$Otherinfo" > "$Nameobject"
 
 # Combine data into a single CSV-file.
-paste -d'\t' "$Regnumbers" "$Days" "$Period" "$Status" "$Blank" "$Thved" "$Fullapplicants" "$Applicants" "$Nameobject" "$Okp" "$Expert" "$StatusInfo"  > "$OutputPath" 
+paste -d'\t' "$Regnumbers" "$Days" "$Period" "$Status" "$Blank" "$Thved" "$Fullapplicants" "$Applicants" "$Nameobject" "$Address" "$Okp" "$Expert" "$StatusInfo" > "$OutputPath" 
 
 # Remove temporary files.
 find $1 -name "*.txt" -not -name "*data*.txt" -type f -delete
